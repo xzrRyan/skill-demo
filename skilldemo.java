cd55e06a2b4832fa39ea6283c71a8ec67d756120
Zhiran Xie <z4xie@ucsd.edu>
2022-05-01 23:23:02 -0700

    failed

--- skilldemo.java
+++ skilldemo.java
@@ -1,2 +1,6 @@
 public class skilldemo{
+    public static int turnAllinto5(int parameter){
+        parameter=5;
+        return parameter;
+    }
 }
---
+++ testdemo.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class testdemo {
+    @Test
+    public void testcase(){
+        assertEquals(3, skilldemo.turnAllinto5(5));
+
+    }
+}
